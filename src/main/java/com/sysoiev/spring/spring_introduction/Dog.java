2585bafe2860f803690236f96c76816badd68cef
Bada1208 <sysoievbogdan@gmail.com>
2021-03-22 17:33:51 +0100

    properties file

--- src/main/java/com/sysoiev/spring/spring_introduction/Dog.java
+++ src/main/java/com/sysoiev/spring/spring_introduction/Dog.java
@@ -1,9 +1,18 @@
 package com.sysoiev.spring.spring_introduction;
 
 public class Dog implements Pet{
+    private String name;
 
     public Dog() {
         System.out.println("Dog is created");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
--- src/main/java/com/sysoiev/spring/spring_introduction/Test4.java
+++ src/main/java/com/sysoiev/spring/spring_introduction/Test4.java
@@ -1,4 +1,16 @@
 package com.sysoiev.spring.spring_introduction;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test4 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
+        Dog dog = context.getBean("myPet", Dog.class);
+        dog.setName("Belka");
+        Dog dog2 = context.getBean("myPet", Dog.class);
+        dog2.setName("Strelka");
+        System.out.println(dog == dog2);
+        System.out.println(dog.getName());
+        System.out.println(dog2.getName());
+    }
 }
