b79288716dfc80eeb569441b6b213be5a8077e4c
Bada1208 <sysoievbogdan@gmail.com>
2021-04-01 13:37:29 +0200

    aop @Before

---
+++ src/main/java/com/sysoiev/spring/aop/MyConfig.java
@@ -0,0 +1,11 @@
+package com.sysoiev.spring.aop;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@ComponentScan("com.sysoiev.spring.aop")
+@EnableAspectJAutoProxy
+public class MyConfig {
+}
